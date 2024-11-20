ebd067f5b4c1f5ecd099fabe51ae3d8e21a84799
Adebayo Adeoye <adeoyeadebayo18@gmail.com>
2024-11-20 14:15:50 +0100

    Implemented Rate Limiting (10 requests per minute)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { FxqlStatementsModule } from './fxql-statements/fxql-statements.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule, FxqlStatementsModule],
+  imports: [
+    DatabaseModule,
+    FxqlStatementsModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
